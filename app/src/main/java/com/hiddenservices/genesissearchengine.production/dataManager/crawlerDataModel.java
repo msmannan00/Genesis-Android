c6e9180e5dec6d4e48107da87117b71b5d59e18e
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-02-13 02:41:31 +0500

    Bug Fixes
    Bug Fixes

--- app/src/main/java/com/hiddenservices/genesissearchengine.production/dataManager/crawlerDataModel.java
+++ app/src/main/java/com/hiddenservices/genesissearchengine.production/dataManager/crawlerDataModel.java
@@ -1,14 +1,11 @@
 package com.hiddenservices.genesissearchengine.production.dataManager;
 
 import android.annotation.SuppressLint;
-import android.text.Html;
-
+import android.text.TextUtils;
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpStack;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -17,46 +14,20 @@ import com.hiddenservices.genesissearchengine.production.constants.strings;
 import com.hiddenservices.genesissearchengine.production.dataManager.models.crawlerRowModel;
 import com.hiddenservices.genesissearchengine.production.helperManager.helperMethod;
 
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.apache.commons.text.StringEscapeUtils;
 import org.mozilla.thirdparty.com.google.android.exoplayer2.util.Log;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
 import java.net.Proxy;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import ch.boye.httpclientandroidlib.NameValuePair;
-import ch.boye.httpclientandroidlib.client.methods.CloseableHttpResponse;
-import ch.boye.httpclientandroidlib.client.methods.HttpUriRequest;
-import ch.boye.httpclientandroidlib.client.methods.RequestBuilder;
-import ch.boye.httpclientandroidlib.impl.client.CloseableHttpClient;
-import ch.boye.httpclientandroidlib.impl.client.HttpClients;
-import ch.boye.httpclientandroidlib.message.BasicNameValuePair;
-import ch.boye.httpclientandroidlib.util.EntityUtils;
 
 @SuppressLint("CommitPrefEdits")
 class crawlerDataModel
@@ -111,20 +82,28 @@ class crawlerDataModel
             String mURL = strings.GENERIC_EMPTY_STR;
 
             private void onSendRequest(){
-                RequestQueue mRequestQueue = Volley.newRequestQueue(mContext, new ProxiedHurlStack());
+                RequestQueue mRequestQueue = Volley.newRequestQueue(mContext);
+
                 String url = "http://trcip42ymcgvv5hsa7nxpwdnott46ebomnn5pm5lovg5hpszyo4n35yd.onion/user_index/";
                 StringRequest mRequestData = new StringRequest(Request.Method.POST, url, response -> {
                     Log.d("",response);
                 }, error -> {
-                    Log.d("",error.toString());
+                     Log.d("",error.toString());
                 }) {
                     protected Map<String, String> getParams() {
+                        mHtml = StringEscapeUtils.escapeXml11(mHtml);
+                        mHtml = mHtml.replace("\n"," ");
+
                         Map<String, String> MyData = new HashMap<>();
                         MyData.put("m_html", mHtml);
                         MyData.put("m_url", mURL);
                         return MyData;
                     }
                 };
+                mRequestData.setRetryPolicy(new DefaultRetryPolicy(
+                        30000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
                 mRequestQueue.add(mRequestData);
             }
 
